8a380919077cc25dbad25c3eb1cb6d8308d36f8f
neetjee032-Debanjan <neetjee032@gmail.com>
2026-06-09 17:00:42 +0530

    Update newton-raphson.js

--- sims/newton-raphson.js
+++ sims/newton-raphson.js
@@ -1,38 +1,40 @@
 export function runNewton(container) {
-  container.innerHTML = `
-    <h3>Newton-Raphson Simulator</h3>
 
-    <input id="x0" placeholder="Initial guess (x0)" />
+  container.innerHTML = `
+    <h3>Newton-Raphson Simulation</h3>
+
+    <input id="x0" placeholder="Initial guess" />
     <button id="run">Run</button>
 
     <canvas id="chart"></canvas>
   `;
 
   document.getElementById("run").onclick = function () {
+
     let x = parseFloat(document.getElementById("x0").value);
 
-    const data = [];
-
     function f(x) {
-      return x*x - 4; // example function
+      return x*x - 4;
     }
 
     function df(x) {
       return 2*x;
     }
 
+    const values = [];
+
     for (let i = 0; i < 10; i++) {
       x = x - f(x)/df(x);
-      data.push(x);
+      values.push(x);
     }
 
     new Chart(document.getElementById("chart"), {
       type: "line",
       data: {
-        labels: data.map((_, i) => i),
+        labels: values.map((_, i) => i),
         datasets: [{
           label: "Convergence",
-          data: data
+          data: values
         }]
       }
     });
