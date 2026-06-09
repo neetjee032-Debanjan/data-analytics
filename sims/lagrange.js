40df3ea0fb21c8ee44ecd0fe28c9e6e5da7b06d3
neetjee032-Debanjan <neetjee032@gmail.com>
2026-06-10 00:43:57 +0530

    Update lagrange.js

--- sims/lagrange.js
+++ sims/lagrange.js
@@ -1,58 +1,51 @@
-export function runLagrange(container) {
+export function runLagrange(container){
 
-  container.innerHTML = `
-    <h3>Lagrange Interpolation (Interactive)</h3>
+container.innerHTML = `
+<div style="color:white;padding:15px;font-family:Arial;">
+  <h2>Lagrange Interpolation</h2>
 
-    <p>Points: (1,1), (2,4), (3,9)</p>
+  <textarea id="points">0,1\n1,3\n2,2</textarea>
 
-    <button id="run">Generate Curve</button>
+  <label>x to estimate:</label>
+  <input id="x" type="number" value="1.5"/>
 
-    <canvas id="chart"></canvas>
-  `;
+  <button id="run">Run</button>
 
-  document.getElementById("run").onclick = function () {
+  <h3 id="result"></h3>
+</div>
+`;
 
-    const x = [1,2,3];
-    const y = [1,4,9];
+function parsePoints(text){
+  return text.trim().split("\n").map(line=>{
+    let [x,y]=line.split(",").map(Number);
+    return {x,y};
+  });
+}
 
-    function L(xi) {
+container.querySelector("#run").onclick = () => {
 
-      let sum = 0;
+  let pts = parsePoints(container.querySelector("#points").value);
+  let x = +container.querySelector("#x").value;
 
-      for (let i = 0; i < x.length; i++) {
-        let term = y[i];
+  let result = 0;
 
-        for (let j = 0; j < x.length; j++) {
-          if (i !== j) {
-            term *= (xi - x[j]) / (x[i] - x[j]);
-          }
-        }
+  for(let i=0;i<pts.length;i++){
 
-        sum += term;
+    let xi = pts[i].x;
+    let yi = pts[i].y;
+
+    let Li = 1;
+
+    for(let j=0;j<pts.length;j++){
+      if(i!==j){
+        Li *= (x - pts[j].x)/(xi - pts[j].x);
       }
-
-      return sum;
     }
 
-    const xs = [];
-    const ys = [];
+    result += yi * Li;
+  }
 
-    for (let i = 0; i < 60; i++) {
-      let xi = 1 + i * 0.03;
-      xs.push(xi);
-      ys.push(L(xi));
-    }
-
-    new Chart(document.getElementById("chart"), {
-      type: "line",
-      data: {
-        labels: xs,
-        datasets: [{
-          label: "Interpolation Curve",
-          data: ys,
-          borderColor: "#a855f7"
-        }]
-      }
-    });
-  };
+  container.querySelector("#result").innerHTML =
+    `Interpolated Value: ${result.toFixed(6)}`;
+};
 }
