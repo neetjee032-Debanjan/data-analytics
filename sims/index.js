8b4b4f63250a006d63c8bd30b4b45c010ce42375
neetjee032-Debanjan <neetjee032@gmail.com>
2026-06-10 00:14:25 +0530

    Update index.js

--- sims/index.js
+++ sims/index.js
@@ -1,7 +1,30 @@
 import { runNewton } from "./newton-raphson.js";
 
+/* Future-ready imports (safe even if file exists later) */
+import { runBisection } from "./bisection.js";
+import { runFalsePosition } from "./false-position.js";
+import { runFixedPoint } from "./fixed-point.js";
+import { runSecant } from "./secant.js";
+
+/**
+ * CENTRAL SIM REGISTRY (THIS IS THE HEART OF YOUR SYSTEM)
+ */
 export const Simulations = {
+
+  // Newton family
   "newton": runNewton,
   "newton-raphson": runNewton,
-  "newtonraphson": runNewton
+  "newtonraphson": runNewton,
+
+  // Root finding methods
+  "bisection": runBisection,
+  "false-position": runFalsePosition,
+  "falseposition": runFalsePosition,
+  "regula-falsi": runFalsePosition,
+
+  "fixed-point": runFixedPoint,
+  "fixedpoint": runFixedPoint,
+
+  "secant": runSecant,
+
 };
