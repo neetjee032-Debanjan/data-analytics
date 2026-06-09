d32529313c9bb79472b2091d59cf480a41f0d388
neetjee032-Debanjan <neetjee032@gmail.com>
2026-06-10 00:28:02 +0530

    Update index.js

--- sims/index.js
+++ sims/index.js
@@ -1,37 +1,10 @@
 import { runNewton } from "./newton-raphson.js";
+import { runBisection } from "./bisection.js";
 
 export const Simulations = {
-
   "newton": runNewton,
   "newton-raphson": runNewton,
   "newtonraphson": runNewton,
 
-  // SAFE PLACEHOLDERS (NO CRASH IF NOT IMPLEMENTED YET)
-  "bisection": (c) => renderMissing(c, "Bisection Method"),
-  "false-position": (c) => renderMissing(c, "False Position Method"),
-  "falseposition": (c) => renderMissing(c, "False Position Method"),
-  "regula-falsi": (c) => renderMissing(c, "Regula Falsi Method"),
-
-  "fixed-point": (c) => renderMissing(c, "Fixed Point Iteration"),
-  "fixedpoint": (c) => renderMissing(c, "Fixed Point Iteration"),
-
-  "secant": (c) => renderMissing(c, "Secant Method"),
+  "bisection": runBisection,
 };
-
-function renderMissing(container, name) {
-  container.innerHTML = `
-    <div style="
-      padding:20px;
-      color:#ff4d4d;
-      font-family:Arial;
-      background:#111827;
-      border-radius:10px;
-    ">
-      <h2>${name}</h2>
-      <p><b>Simulation not implemented yet.</b></p>
-      <p style="opacity:0.8">
-        This method will be added in next upgrade phase.
-      </p>
-    </div>
-  `;
-}
