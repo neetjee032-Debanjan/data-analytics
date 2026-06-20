356e2af369f354736d99565e1eff84ad46ea56df
neetjee032-Debanjan <neetjee032@gmail.com>
2026-06-20 19:30:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1223,3 +1223,21 @@ window.addEventListener(
 
   }
 );
+
+const glow = document.createElement("div");
+
+glow.className = "cursor-glow";
+
+document.body.appendChild(glow);
+
+document.addEventListener(
+  "mousemove",
+  (e)=>{
+
+    glow.style.left =
+      e.clientX + "px";
+
+    glow.style.top =
+      e.clientY + "px";
+  }
+);
